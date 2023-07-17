9b4b1a796297942f28d9f5927abbf017b72d9eae
Sinbanbonn <logvinov0607@icloud.com>
2023-07-17 14:30:01 +0300

    Project was started

---
+++ Pokemon App/Pokemon App/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon App
+//
+//  Created by Андрей Логвинов on 7/17/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
